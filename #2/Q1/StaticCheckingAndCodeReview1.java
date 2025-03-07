5cf5fb65133c330a4d9e2db5e563c3f335c851ab
Swayam Khatter <swayam.khatter@metacube.com>
2025-03-07 17:11:25 +0530

    try catch in #2 Q1

--- #2/Q1/StaticCheckingAndCodeReview1.java
+++ #2/Q1/StaticCheckingAndCodeReview1.java
@@ -4,19 +4,23 @@ class HexCalc {
     /* this function converts hexadecimal number to decimal number */
     int hexToDecimal(String hex) {
         int decimal = 0;
-        int length = hex.length();
-        for (int index = 0; index < length; index++) {
-            char hexChar = hex.charAt(length - index - 1);
-            int value;
-            if (hexChar >= '0' && hexChar <= '9') { // if char is a number
-                value = hexChar - '0';
-            } else if (hexChar >= 'A' && hexChar <= 'F') { // if char is an alphabet from A-F
-                value = hexChar - 'A' + 10;
-            } else {
-                throw new IllegalArgumentException("Invalid character!\n"); // if the hex string has a character other
-                                                                            // than 0-9 or A-F, throw error
+        try {
+            int length = hex.length();
+            for (int index = 0; index < length; index++) {
+                char hexChar = hex.charAt(length - index - 1);
+                int value;
+                if (hexChar >= '0' && hexChar <= '9') { // if char is a number
+                    value = hexChar - '0';
+                } else if (hexChar >= 'A' && hexChar <= 'F') { // if char is an alphabet from A-F
+                    value = hexChar - 'A' + 10;
+                } else {
+                    throw new IllegalArgumentException("Invalid character!\n"); // if the hex string has a character other
+                                                                                // than 0-9 or A-F, throw error
+                }
+                decimal += value * Math.pow(16, index);
             }
-            decimal += value * Math.pow(16, index);
+        } catch(Exception e) {
+            System.out.println("Error in converting Hexadecimal to Decimal: " + e.getMessage());
         }
         return decimal;
     }
@@ -29,18 +33,22 @@ class HexCalc {
         }
 
         StringBuilder hex = new StringBuilder(); // StringBuilder for the new hexadecimal string
-        while (decimal > 0) {
-            int rem = decimal % 16;
-            char hexChar;
-            if (rem < 10) { // if rem is less than 10 it will be represented by a number in hexadecimal
-                            // string
-                hexChar = (char) ('0' + rem);
-            } else { // if rem is greater then or equal to 10 it will be represented by an alpabet
-                     // (A-F) in hexadecimal string
-                hexChar = (char) ('A' + (rem - 10));
+        try {
+            while (decimal > 0) {
+                int rem = decimal % 16;
+                char hexChar;
+                if (rem < 10) { // if rem is less than 10 it will be represented by a number in hexadecimal
+                                // string
+                    hexChar = (char) ('0' + rem);
+                } else { // if rem is greater then or equal to 10 it will be represented by an alpabet
+                        // (A-F) in hexadecimal string
+                    hexChar = (char) ('A' + (rem - 10));
+                }
+                hex.insert(0, hexChar);
+                decimal = decimal / 16;
             }
-            hex.insert(0, hexChar);
-            decimal = decimal / 16;
+        } catch (Exception e) {
+            System.out.println("Error in converting Decimal to Hexadecimal: " + e.getMessage());
         }
         return hex.length() > 0 ? hex.toString() : "0"; // converting StringBuilder hex to string before returning
     }
@@ -53,10 +61,15 @@ class HexCalc {
      * it
      */
     String add(String hex1, String hex2) {
-        int dec1 = hexToDecimal(hex1);
-        int dec2 = hexToDecimal(hex2);
-        int decResult = dec1 + dec2;
-        String result = decimalToHex(decResult);
+        String result = null;
+        try {
+            int dec1 = hexToDecimal(hex1);
+            int dec2 = hexToDecimal(hex2);
+            int decResult = dec1 + dec2;
+            result = decimalToHex(decResult);
+        } catch (Exception e) {
+            System.out.println("Error in adding Hexadecimal Strings: " + e.getMessage());
+        }
         return result;
     }
 
@@ -68,13 +81,18 @@ class HexCalc {
      * it
      */
     String subtract(String hex1, String hex2) {
-        int dec1 = hexToDecimal(hex1);
-        int dec2 = hexToDecimal(hex2);
-        if (dec1 < dec2) {
-            throw new ArithmeticException("-ve number can't compute");
+        String result = null;
+        try {
+            int dec1 = hexToDecimal(hex1);
+            int dec2 = hexToDecimal(hex2);
+            if (dec1 < dec2) {
+                throw new ArithmeticException("-ve number can't compute");
+            }
+            int decResult = dec1 - dec2;
+            result = decimalToHex(decResult);
+        } catch (Exception e) {
+            System.out.println("Error in subtracting Hexadecimal Strings: " + e.getMessage());
         }
-        int decResult = dec1 - dec2;
-        String result = decimalToHex(decResult);
         return result;
     }
 
@@ -86,10 +104,15 @@ class HexCalc {
      * it
      */
     String multiply(String hex1, String hex2) {
-        int dec1 = hexToDecimal(hex1);
-        int dec2 = hexToDecimal(hex2);
-        int decResult = dec1 * dec2;
-        String result = decimalToHex(decResult);
+        String result = null;
+        try {
+            int dec1 = hexToDecimal(hex1);
+            int dec2 = hexToDecimal(hex2);
+            int decResult = dec1 * dec2;
+            result = decimalToHex(decResult);
+        } catch (Exception e) {
+            System.out.println("Error in multiplying Hexadecimal Strings: " + e.getMessage());
+        }
         return result;
     }
 
@@ -101,16 +124,21 @@ class HexCalc {
      * it
      */
     String divide(String hex1, String hex2) {
-        int dec1 = hexToDecimal(hex1);
-        int dec2 = hexToDecimal(hex2);
-        if (dec2 == 0) {
-            throw new ArithmeticException("Cannot divide by Zero!!\n");
+        String result = null;
+        try {
+            int dec1 = hexToDecimal(hex1);
+            int dec2 = hexToDecimal(hex2);
+            if (dec2 == 0) {
+                throw new ArithmeticException("Cannot divide by Zero!!\n");
+            }
+            if(dec1%dec2 != 0) {
+                throw new ArithmeticException("Result must be an Integer!!\n");
+            }
+            int decResult = dec1 / dec2;
+            result = decimalToHex(decResult);
+        } catch(Exception e) {
+            System.out.println("Error in dividing Hexadecimal Strings: " + e.getMessage());
         }
-        if(dec1%dec2 != 0) {
-            throw new ArithmeticException("Result must be an Integer!!\n");
-        }
-        int decResult = dec1 / dec2;
-        String result = decimalToHex(decResult);
         return result;
     }
 

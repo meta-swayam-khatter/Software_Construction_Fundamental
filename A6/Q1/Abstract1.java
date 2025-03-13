3ab121d43104f924badc26606be3fcc3cfe51cdb
Swayam Khatter <swayam.khatter@metacube.com>
2025-03-13 12:19:55 +0530

    A6 done

--- A6/Q1/Abstract1.java
+++ A6/Q1/Abstract1.java
@@ -2,10 +2,11 @@ package A6.Q1;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
 
 final class IntSet {
     private boolean[] elementsPresent;
-    private int maxValue = 1000;
+    private static int maxValue = 1000;
 
     /**
      * this function logs errors in error.txt file
@@ -19,6 +20,10 @@ final class IntSet {
         }
     }
 
+    /**
+     * Constructor
+     * @param arr input array which will be converted to set
+     */
     IntSet(int[] arr) {
         elementsPresent = new boolean[maxValue+1];
         for(int index=0; index<arr.length; index++) {
@@ -28,6 +33,9 @@ final class IntSet {
         }
     }
 
+    /**
+     * this function prints the set
+     */
     void printSet() {
         for(int i=1; i<=maxValue; i++) {
             if(this.elementsPresent[i]) {
@@ -36,6 +44,11 @@ final class IntSet {
         }
     }
 
+    /**
+     * this function checks if a number is present in the set or not
+     * @param num number which we want to check
+     * @return true if present, false otherwise
+     */
     boolean isMember(int num) {
         try {
             if(num>1000 || num<=0) {
@@ -51,6 +64,10 @@ final class IntSet {
         }
     }
 
+    /**
+     * this function finds the size of the set
+     * @return size of the set
+     */
     int size() {
         try {
             int count=0;
@@ -66,6 +83,11 @@ final class IntSet {
         }
     }
 
+    /**
+     * this function checks if the given set is a subset or not
+     * @param subSet subset that we want to check
+     * @return true if subSet is a subset, false otherwise
+     */
     boolean isSubSet(IntSet subSet) {
         try {
             for(int index=1; index<=maxValue; index++) {
@@ -80,6 +102,10 @@ final class IntSet {
         }
     }
 
+    /**
+     * this function finds the complement of a set (considering universal set to be 1-1000)
+     * @return complement of a set
+     */
     IntSet getComplement() {
         try {
             IntSet complementArr = new IntSet(new int[]{});
@@ -93,7 +119,13 @@ final class IntSet {
         }
     }
 
-    IntSet getUnion(IntSet set1, IntSet set2) {
+    /**
+     * this function finds the union of a two sets
+     * @param set1 first set
+     * @param set2 second set
+     * @return union set
+     */
+    static IntSet getUnion(IntSet set1, IntSet set2) {
         try {
             IntSet result = new IntSet(new int[]{});
             for(int index=1; index<=maxValue; index++) {
@@ -108,7 +140,13 @@ final class IntSet {
         }
     }
 
-    IntSet getIntersection(IntSet set1, IntSet set2) {
+    /**
+     * this function finds the intersention of two sets
+     * @param set1 fisrt set
+     * @param set2 second set
+     * @return intersection set
+     */
+    static IntSet getIntersection(IntSet set1, IntSet set2) {
         try {
             IntSet result = new IntSet(new int[]{});
             for(int index=1; index<=maxValue; index++) {
@@ -123,7 +161,13 @@ final class IntSet {
         }
     }
 
-    IntSet getDifference(IntSet set1, IntSet set2) {
+    /**
+     * this function finds the difference of two sets
+     * @param set1 first set
+     * @param set2 second set
+     * @return differnce of first and seconds set
+     */
+    static IntSet getDifference(IntSet set1, IntSet set2) {
         try {
             IntSet result = new IntSet(new int[]{});
             for(int index=1; index<=maxValue; index++) {
@@ -141,12 +185,125 @@ final class IntSet {
 
 public class Abstract1 {
     public static void main(String[] args) {
-        IntSet obj1 = new IntSet(new int[]{2,45,55,565,24,66,78});
-        IntSet obj2 = new IntSet(new int[]{45,24,78});
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the first set: ");
+        int num = sc.nextInt();
+        
+        System.out.println("Enter the elements of set: ");
+        int[] arr = new int[num];
+        for(int index=0; index<num; index++) {
+            int value = sc.nextInt();
+            arr[index] = value;
+        }
+        IntSet obj1 = new IntSet(arr);
 
-        IntSet unionArr = new IntSet(new int[]{});
-        unionArr = unionArr.getUnion(obj1, obj2);
+        while(true) {
+            System.out.println("\nEnter your choice: \n1. Check is member\n2. Find size\n3. check is subset\n4. Get union\n5. Get intersection\n6. Get complement\n7. Get difference\n 0. Exit");
+            int choice = sc.nextInt();
 
-        unionArr.printSet();
+            if(choice == 0) {
+                System.out.println("Exiting........\n");
+                break;
+            }
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the element you want to check is member: ");
+                    int elem = sc.nextInt();
+                    if(obj1.isMember(elem)) {
+                        System.out.println(elem + " is a member of the Set\n");
+                    } else {
+                        System.out.println(elem + " is not a member of the Set\n");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Size of the set is: " + obj1.size() + "\n");
+                    break;
+
+                case 3:
+                    System.out.println("Enter the number of elements in the second set: ");
+                    num = sc.nextInt();
+                    
+                    System.out.println("Enter the elements of the second set: ");
+                    arr = new int[num];
+                    for(int index=0; index<num; index++) {
+                        int value = sc.nextInt();
+                        arr[index] = value;
+                    }
+                    IntSet obj2 = new IntSet(arr);
+                    if(obj1.isSubSet(obj2)) {
+                        System.out.println("This set is a subset of the first set\n");
+                    } else {
+                        System.out.println("This set is not a subset of first set\n");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Enter the number of elements in the second set: ");
+                    num = sc.nextInt();
+                    
+                    System.out.println("Enter the elements of the second set: ");
+                    arr = new int[num];
+                    for(int index=0; index<num; index++) {
+                        int value = sc.nextInt();
+                        arr[index] = value;
+                    }
+                    obj2 = new IntSet(arr);
+                    IntSet union = IntSet.getUnion(obj1, obj2);
+                    System.out.println("The union set is: ");
+                    union.printSet();
+                    break;
+
+                case 5:
+                    System.out.println("Enter the number of elements in the second set: ");
+                    num = sc.nextInt();
+                    
+                    System.out.println("Enter the elements of the second set: ");
+                    arr = new int[num];
+                    for(int index=0; index<num; index++) {
+                        int value = sc.nextInt();
+                        arr[index] = value;
+                    }
+                    obj2 = new IntSet(arr);
+                    IntSet intersection = IntSet.getIntersection(obj1, obj2);
+                    System.out.println("The intersection set is: ");
+                    intersection.printSet();
+                    break;
+
+                case 6:
+                    IntSet complement = obj1.getComplement();
+                    System.out.println("The complement set is: ");
+                    complement.printSet();
+                    break;
+
+                case 7:
+                    System.out.println("Enter the number of elements in the second set: ");
+                    num = sc.nextInt();
+                    
+                    System.out.println("Enter the elements of the second set: ");
+                    arr = new int[num];
+                    for(int index=0; index<num; index++) {
+                        int value = sc.nextInt();
+                        arr[index] = value;
+                    }
+                    obj2 = new IntSet(arr);
+                    IntSet difference = IntSet.getDifference(obj1, obj2);
+                    System.out.println("The difference of these two sets is: ");
+                    difference.printSet();
+                    break;
+                
+                default:
+                    System.out.println("Enter a valid choice!!\n");
+                    break;
+            }
+        }
+        // IntSet obj1 = new IntSet(new int[]{2,45,55,565,24,66,78});
+        // IntSet obj2 = new IntSet(new int[]{45,24,78});
+
+        // IntSet unionArr = new IntSet(new int[]{});
+        // unionArr = IntSet.getUnion(obj1, obj2);
+
+        // unionArr.printSet();
     }
 }
---
+++ A6/Q2/Abstract2.java
@@ -0,0 +1,246 @@
+package A6.Q2;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+class Poly {
+    private final int[][] terms;
+
+    public Poly(int[][] terms) {
+        this.terms = terms;
+    }
+
+    /**
+     * this function logs errors in error.txt file
+     * @param e exception
+     */
+    private static void logError(Exception e) {
+        try(FileWriter fw = new FileWriter("error.txt", true)) {
+            fw.write(e.getMessage() + "\n");
+        } catch(IOException ioException) {
+            System.out.println("Failed to log the error!!");
+        }
+    }
+
+    /**
+     * this function prints the polynomial expression
+     */
+    public void printPoly() {
+        try {
+            for(int[] term : terms) {
+                System.out.print(term[0] + "x^" + term[1] + " + ");
+            }
+            return;
+        } catch (Exception e) {
+            logError(e);
+            return;
+        }
+    }
+
+    /**
+     * this function evaluates the polynomial expression based on the value of x
+     * @param x value for which the polynomial has to be evaluated
+     * @return evaluated result of the polynomial
+     */
+    public float evaluate(float x) {
+        try {
+            float sum=0;
+            for(int[] term:terms) {
+                sum += term[0] * Math.pow(x, term[1]);
+            }
+            return sum;
+        } catch(Exception e) {
+            logError(e);
+            return -1;
+        }
+    }
+
+    /**
+     * this function finds the highest degree present in the polynomail
+     * @return highest degree in the polynomial
+     */
+    public int degree() {
+        try {
+            int maxDegree=0;
+            for(int[] term:terms) {
+                if(term[1] > maxDegree) {
+                    maxDegree = term[1];
+                }
+            }
+            return maxDegree;
+        } catch (Exception e) {
+            logError(e);
+            return -1;
+        }
+    }
+
+    /**
+     * this function add the two polynomials
+     * @param p1 first polynomial
+     * @param p2 second polynomial
+     * @return sum of two polynomials
+     */
+    public static Poly addPoly(Poly p1, Poly p2) {
+        try {
+            Set<Integer> exponents = new HashSet<>();
+            for(int[] term : p1.terms) {
+                exponents.add(term[1]);
+            }
+            for(int[] term : p2.terms) {
+                exponents.add(term[1]);
+            }
+            int[][] result = new int[exponents.size()][2];
+            int index=0;
+            for(int exp : exponents) {
+                int coef=0;
+                for(int[] term : p1.terms) {
+                    if(term[1] == exp) coef += term[0]; 
+                }
+                for(int[] term : p2.terms) {
+                    if(term[1] == exp) coef += term[0]; 
+                }
+                result[index++] = new int[]{coef, exp};
+            }
+            return new Poly(result);
+        } catch (Exception e) {
+            logError(e);
+            return new Poly(new int[][]{});
+        }
+    }
+
+    /**
+     * this function multiples the two polynomials
+     * @param p1 first polynomial
+     * @param p2 second polynomial 
+     * @return product of two polynomials
+     */
+    public static Poly multiplyPoly(Poly p1, Poly p2) {
+        try {
+            int[][] product = new int[p1.terms.length * p2.terms.length][2];
+            int index=0;
+            for(int[] term1 : p1.terms) {
+                for(int[] term2 : p2.terms) {
+                    product[index++] = new int[]{term1[0] * term2[0], term1[1] + term2[1]};
+                }
+            }
+            Set<Integer> exponents = new HashSet<>();
+            for(int[] prod : product) {
+                exponents.add(prod[1]);
+            }
+            int[][] result = new int[exponents.size()][2];
+            index=0;
+            for(int exp : exponents) {
+                int coef = 0;
+                for(int[] prod : product) {
+                    if(prod[1] == exp) coef += prod[0]; 
+                }
+                result[index++] = new int[]{coef, exp};
+            }
+            return new Poly(result);
+        } catch (Exception e) {
+            logError(e);
+            return new Poly(new int[][]{});
+        }
+    }
+}
+
+public class Abstract2 {
+    public static void main(String[] args) {
+        // int[][] terms ={{1,1}, {3,2}, {5,3}};
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of entries: ");
+        int num = sc.nextInt();
+        int[][] temp = new int[num][2];
+        for(int index=0; index<num; index++) {
+            System.out.print("Coefficient: ");
+            int coef = sc.nextInt();
+            System.out.print("Exponent: ");
+            int exp = sc.nextInt();
+
+            temp[index][0] = coef;
+            temp[index][1] = exp;
+        }
+        Poly obj1 = new Poly(temp);
+
+        while(true) {
+            System.out.println("\nEnter your choice what you want to do: \n1. Evaluate\n2. Find highest degree\n3. Add 2 polynomials\n4. Multiply 2 polynomials\n0. Exit\n");
+            int choice = sc.nextInt();
+
+            if(choice == 0) {
+                System.out.println("Exiting..........\n");
+                break;
+            }
+
+            switch(choice) {
+                case 1:
+                    System.out.println("Enter the value of x for which you want to evaluate: ");
+                    int x = sc.nextInt();
+
+                    System.out.println("Evaluated result: " + obj1.evaluate(x));
+                    break;
+
+                case 2:
+                    System.out.println("Highest degree present in the polynomail is: " + obj1.degree());
+                    break;
+
+                case 3:
+                    System.out.println("Enter the number of entries for the 2nd polynomial: ");
+                    num = sc.nextInt();
+                    temp = new int[num][2];
+                    for(int index=0; index<num; index++) {
+                        System.out.print("Coefficient: ");
+                        int coef = sc.nextInt();
+                        System.out.print("Exponent: ");
+                        int exp = sc.nextInt();
+            
+                        temp[index][0] = coef;
+                        temp[index][1] = exp;
+                    }
+                    Poly obj2 = new Poly(temp);
+
+                    Poly sum = Poly.addPoly(obj1, obj2);
+                    System.out.print("Resultant polygon after adding : ");
+                    obj1.printPoly();
+                    System.out.print(" and ");
+                    obj2.printPoly();
+                    System.out.println(" is: ");
+                    sum.printPoly();
+                    break;
+
+                case 4:
+                    System.out.println("Enter the number of entries for the 2nd polynomial: ");
+                    num = sc.nextInt();
+                    temp = new int[num][2];
+                    for(int index=0; index<num; index++) {
+                        System.out.print("Coefficient: ");
+                        int coef = sc.nextInt();
+                        System.out.print("Exponent: ");
+                        int exp = sc.nextInt();
+            
+                        temp[index][0] = coef;
+                        temp[index][1] = exp;
+                    }
+                    obj2 = new Poly(temp);
+
+                    Poly product = Poly.multiplyPoly(obj1, obj2);
+                    System.out.print("Resultant polygon after multiplying : ");
+                    obj1.printPoly();
+                    System.out.print(" and ");
+                    obj2.printPoly();
+                    System.out.println(" is: ");
+                    product.printPoly();
+                    break;
+
+                default:
+                    System.out.println("Enter a valid input!!\n");
+                    break;
+            }
+        }
+
+        System.out.println(obj1.degree());
+        sc.close();
+    }
+}
